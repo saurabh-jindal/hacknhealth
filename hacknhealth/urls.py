f00624241498b30dc1d0d00178dacbac682d81a7
Saurabh Jindal <jindalsaurabh533@gmail.com>
2020-06-15 12:50:59 +0530

    tinymce integrated

--- hacknhealth/urls.py
+++ hacknhealth/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+admin.site.site_name = "HackHHealth"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
